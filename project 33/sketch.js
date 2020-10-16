c3a36cd5f548c3d383a743fcb422e0cbf7a92f56
Pihu Tiwari <70630802+Pihu2008@users.noreply.github.com>
2020-10-16 12:47:38 +0530

    Add files via upload

---
+++ project 33/sketch.js
@@ -0,0 +1,104 @@
+var Engine = Matter.Engine,
+  World = Matter.World,
+  Events = Matter.Events,
+  Bodies = Matter.Bodies;
+ var divisions = [];
+//var particles = [];
+var plinkos = [];
+var score = 0;
+var particle;
+var turn = 0;
+var gameState;
+
+var divisionHeight=300;
+var score =0;
+function setup() {
+  createCanvas(800, 800);
+  engine = Engine.create();
+  world = engine.world;
+  ground = new Ground(width/2,height,width,20);
+
+   for (var k = 0; k <=width; k = k + 80) {
+     divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
+   }
+
+
+    for (var j = 75; j <=width; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,75));
+    }
+
+    for (var j = 50; j <=width-10; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,175));
+    }
+
+     for (var j = 75; j <=width; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,275));
+    }
+
+     for (var j = 50; j <=width-10; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,375));
+    }
+
+    
+
+    
+}
+ 
+
+
+function draw() {
+  background("black");
+  
+  Engine.update(engine);
+ 
+  strokeWeight(4);
+  fill("white");
+  textSize(40);
+  text("score"+score,20,30);
+  textSize(30);
+  text("500",10,550);
+  text("500",90,550);
+  text("500",170,550);
+  text("500",250,550);
+  text("100",330,550);
+  text("100",410,550);
+  text("100",490,550);
+  text("200",570,550);
+  text("200",650,550);
+  text("200",730,550);
+
+   for (var i = 0; i < plinkos.length; i++) {
+     
+     plinkos[i].display();
+     
+   }
+
+  // if(frameCount%60===0){
+   //  particles.push(new Particle(random(width/2-30, width/2+30), 10,10));
+    // score++;
+  // }
+  for (var j = 0; j < particle.length; j++) {
+   
+    particle[j].display();
+  }
+   for (var k = 0; k < divisions.length; k++) {
+     
+     divisions[k].display();
+   }
+   mousePressed();
+}
+function mousePressed()
+{
+  if(gameState === "end"){
+    score++;
+    particle = new Particle(mouseX,10,10,10);
+  }
+}
